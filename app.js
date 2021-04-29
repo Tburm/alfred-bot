2b3f511663d5212018710a71308482d21d9994a2
troy <troy.burmeister@gmail.com>
2021-04-29 17:14:42 -0600

    add server

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const Discord = require('discord.js');
-const Twit = require('twit');
+var http = require('http');
 require('dotenv').config();
 
 const client = new Discord.Client({
   partials: ['MESSAGE', 'REACTION', 'CHANNEL'],
 });
 
-client.login(process.env.BOT_TOKEN);
+client.on('ready', () => {
+    console.log('The Bot is ready!')
+});
 
-client.on('ready', () => console.log('The Bot is ready!'));
-
-// Adding a response
+// A simple response
 client.on('message', (msg) => {
   if (msg.content === 'Alfred?') {
     msg.channel.send('Yes?');
   }
 });
+
+client.login(process.env.BOT_TOKEN);
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(8000, 'localhost');
