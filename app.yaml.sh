7018d1909fc21913123e61d5fad2e40f6e22d328
troy <troy.burmeister@gmail.com>
2021-04-30 13:34:49 -0600

    no idle instances

--- app.yaml.sh
+++ app.yaml.sh
@@ -5,8 +5,7 @@ instance_class: F1
 automatic_scaling:
   max_instances: 1
   min_instances: 1
-  min_idle_instances: 1
-  max_idle_instances: 1
+  max_idle_instances: 0
 env_variables:
   GCLOUD_PROJECT: \"$GCLOUD_PROJECT\"
   BOT_TOKEN: \"$BOT_TOKEN\"
