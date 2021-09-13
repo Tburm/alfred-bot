4b4cbe057f3f1389a801f8521483befcaba1d0a1
Troy Burmeister <troy.burmeister@ibotta.com>
2021-09-13 16:08:44 -0600

    lower minimum instances

--- app.yaml.sh
+++ app.yaml.sh
@@ -4,7 +4,7 @@ runtime: nodejs14
 instance_class: F1
 automatic_scaling:
   max_instances: 1
-  min_instances: 1
+  min_instances: 0
   max_idle_instances: 0
 env_variables:
   GCLOUD_PROJECT: \"$GCLOUD_PROJECT\"
